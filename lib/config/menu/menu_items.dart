35edc7e503e17ff2141bd76983500e22151d3052
jebarcha <jebarcha@hotmail.com>
2023-05-25 12:41:32 -0600

    added app_theme styles of appbar

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem ({
+    required this.title, 
+    required this.subTitle, 
+    required this.link, 
+    required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+
+  MenuItem(
+      title: 'Buttons', 
+      subTitle: 'Multiple buttons in Flutter', 
+      link: '/buttons', 
+      icon: Icons.smart_button_outlined
+  ),
+
+  MenuItem(
+      title: 'Cards', 
+      subTitle: 'Contaier styled', 
+      link: '/card', 
+      icon: Icons.credit_card
+  ),
+
+];
